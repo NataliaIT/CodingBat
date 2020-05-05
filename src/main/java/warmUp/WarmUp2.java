0594d803769fb1449fe4cd2c9a5025f9cebf8235
Natalia <natikrak@gmail.com>
2020-05-05 10:56:23 +0200

    doubleX

--- src/main/java/warmUp/WarmUp2.java
+++ src/main/java/warmUp/WarmUp2.java
@@ -12,12 +12,42 @@ stringTimes("Hi", 1) → "Hi"
      */
 
     public String stringTimes(String str, int n) {
-        String newString= "";
+        String newString = "";
         for (int i = 0; i < n; i++) {
-            newString +=str;
+            newString += str;
         }
         return newString;
 
     }
+
+    /*
+
+Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+
+doubleX("axxbb") → true
+doubleX("axaxax") → false
+doubleX("xxxxx") → true
+     */
+
+    boolean doubleXSolution1(String str) {
+        int indexOfFirstX = str.indexOf('x');
+
+        if ((str.length() < 2) || (indexOfFirstX == str.length() - 1)) return false;
+
+        else if (str.charAt(indexOfFirstX + 1) == 'x') return true;
+        else return false;
+
+    }
+
+    boolean doubleXSOlution2(String str) {
+        int indexOfFirstX = str.indexOf('x');
+        if ((indexOfFirstX == -1) || (indexOfFirstX + 1 >= str.length())) return false;
+
+        String substringStartedWithX = str.substring(indexOfFirstX);
+        return substringStartedWithX.startsWith("xx");
+    }
+
+
+}
 
 }
