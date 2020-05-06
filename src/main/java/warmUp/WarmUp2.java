938b9abf99af2c49b7c43d58762f6757f1edd0c7
Natalia <natikrak@gmail.com>
2020-05-06 19:14:02 +0200

    frontTimes

--- src/main/java/warmUp/WarmUp2.java
+++ src/main/java/warmUp/WarmUp2.java
@@ -145,6 +145,31 @@ noTriples([1, 1, 1, 2, 2, 2, 1]) → false
         return true;
     }
 
+/*
+Given a string and a non-negative int n, we'll say that the front of the string is
+the first 3 chars, or whatever is there if the string is less than length 3.
+Return n copies of the front;
+
+
+frontTimes("Chocolate", 2) → "ChoCho"
+frontTimes("Chocolate", 3) → "ChoChoCho"
+frontTimes("Abc", 3) → "AbcAbcAbc"
+ */
+
+    public String frontTimes(String str, int n) {
+        String front ="";
+        if (str.length()<3){
+            front = str;
+        }
+        else front= str.substring(0,3);
+        String resultStr ="";
+        for (int i = 0; i<n ; i++){
+            resultStr+=front;
+        }
+        return resultStr;
+    }
+
+
 }
 
 
