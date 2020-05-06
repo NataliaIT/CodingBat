360cb790b65e92ebb4c5e8564d96d8fff9f3a957
Natalia <natikrak@gmail.com>
2020-05-06 19:54:30 +0200

    stringMatch

--- src/main/java/warmUp/WarmUp2.java
+++ src/main/java/warmUp/WarmUp2.java
@@ -206,6 +206,38 @@ arrayCount9([1, 9, 9, 3, 9]) → 3
         }
         return count;
     }
+/*
+
+Given 2 strings, a and b, return the number of the positions
+where they contain the same length 2 substring.
+So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az"
+substrings appear in the same place in both strings.
+
+
+stringMatch("xxcaazz", "xxbaaz") → 3
+stringMatch("abc", "abc") → 2
+stringMatch("abc", "axc") → 0
+ */
+
+    public int stringMatch(String a, String b) {
+        int length = a.length();
+
+        int count = 0;
+
+        if (a.length() > b.length()) {
+            length = b.length();
+        }
+
+        if (length < 2) return 0;
+        if (length >= 2) {
+            for (int i = 0; i < length - 1; i++) {
+                if (a.substring(i, i + 2).equals(b.substring(i, i + 2))) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 
 
 }
