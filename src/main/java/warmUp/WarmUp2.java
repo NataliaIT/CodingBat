3ddfdb0f04f2e06e3d52994c76edf865e2030f3e
Natalia <natikrak@gmail.com>
2020-05-07 21:15:43 +0200

    stringSplosion

--- src/main/java/warmUp/WarmUp2.java
+++ src/main/java/warmUp/WarmUp2.java
@@ -314,4 +314,22 @@ countXX("xxxx") → 3
         return count;
     }
 
+    /*
+    Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+
+stringSplosion("Code") → "CCoCodCode"
+stringSplosion("abc") → "aababc"
+stringSplosion("ab") → "aab"
+     */
+
+    public String stringSplosion(String str) {
+        String newString = "";
+        for (int i = 0; i < str.length(); i++) {
+            newString += str.substring(0, i + 1);
+        }
+        return newString;
+    }
+
+
 
