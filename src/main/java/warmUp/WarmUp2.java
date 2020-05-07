cf591b71a6bcd483bb76bb6df095467adedfb5ae
Natalia <natikrak@gmail.com>
2020-05-07 19:44:37 +0200

    stringYak

--- src/main/java/warmUp/WarmUp2.java
+++ src/main/java/warmUp/WarmUp2.java
@@ -239,6 +239,38 @@ stringMatch("abc", "axc") → 0
         return count;
     }
 
+/*
+Suppose the string "yak" is unlucky. Given a string, return
+ a version where all the "yak" are removed, but the "a" can
+  be any char. The "yak" strings will not overlap.
+
+
+stringYak("yakpak") → "pak"
+stringYak("pakyak") → "pak"
+stringYak("yak123ya") → "123ya"
+ */
+
+    public String stringYak(String str) {
+        String resultStr ="";
+        if(str.length() < 3) {
+            return str;
+        }
+
+        if(str.length() >= 3) {
+            for(int i=0; i < str.length(); i++) {
+                if (i+2<str.length() && str.charAt(i)=='y' && str.charAt(i+2)=='k') {
+
+                    i+=2;
+                }
+                else {
+                    resultStr += str.charAt(i);
+                }
+            }
+        }
+        return resultStr;
+    }
+
+
 
 }
 
