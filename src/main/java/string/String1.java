91fa067faf7882d49392b42db15c032f0e5efbf0
Natalia <natikrak@gmail.com>
2020-05-10 15:13:04 +0200

    nonStart

--- src/main/java/string/String1.java
+++ src/main/java/string/String1.java
@@ -47,4 +47,26 @@ firstHalf("abcdef") → "abc"
         return str.substring(0, newLength);
     }
 
+    /*
+    Given 2 strings, return their concatenation, except omit the first char of each.
+    The strings will be at least length 1.
+
+
+nonStart("Hello", "There") → "ellohere"
+nonStart("java", "code") → "avaode"
+nonStart("shotl", "java") → "hotlava"
+     */
+
+    public String nonStart(String a, String b) {
+        String front = "";
+        String end = "";
+        for (int i = 1; i < a.length(); i++) {
+            front += a.charAt(i);
+        }
+        for (int i = 1; i < b.length(); i++) {
+            end += b.charAt(i);
+        }
+        return front + end;
+    }
+
 }
