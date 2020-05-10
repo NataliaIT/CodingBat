84e9c6882473b85f4771c8ca377273af4c7e4497
Natalia <natikrak@gmail.com>
2020-05-10 15:57:41 +0200

    seeColor

--- src/main/java/string/String1.java
+++ src/main/java/string/String1.java
@@ -149,4 +149,22 @@ lastChars("hi", "") → "h@"
         return front + end;
     }
 
+
+    /*
+    Given a string, if the string begins with "red" or "blue" return that color string,
+    otherwise return the empty string.
+
+
+seeColor("redxx") → "red"
+seeColor("xxred") → ""
+seeColor("blueTimes") → "blue"
+     */
+
+    public String seeColor(String str) {
+        if (str.startsWith("red")) {
+            return str.substring(0, 3);
+        } else if (str.startsWith("blue")) {
+            return str.substring(0, 4);
+        } else return "";
+    }
 }
