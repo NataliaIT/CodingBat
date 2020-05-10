48a8190bd2de9144e515ec6fb93632b910257dce
Natalia <natikrak@gmail.com>
2020-05-10 16:01:48 +0200

    extraFront

--- src/main/java/string/String1.java
+++ src/main/java/string/String1.java
@@ -167,4 +167,30 @@ seeColor("blueTimes") → "blue"
             return str.substring(0, 4);
         } else return "";
     }
+
+    /*
+
+Given a string, return a new string made of 3 copies of the first 2 chars
+ of the original string. The string may be any length.
+ If there are fewer than 2 chars, use whatever is there.
+
+
+extraFront("Hello") → "HeHeHe"
+extraFront("ab") → "ababab"
+extraFront("H") → "HHH"
+     */
+
+
+    public String extraFront(String str) {
+        String newStr = "";
+        if (str.length() < 2) {
+            newStr = str + str + str;
+        } else {
+            String temp = str.substring(0, 2);
+            newStr = temp + temp + temp;
+        }
+
+        return newStr;
+    }
+
 }
