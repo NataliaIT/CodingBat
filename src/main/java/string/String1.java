62ad3fff48ec6aee112cf3c3bd1f2da69f48f484
Natalia <natikrak@gmail.com>
2020-05-11 17:08:00 +0200

    hasBad

--- src/main/java/string/String1.java
+++ src/main/java/string/String1.java
@@ -315,5 +315,22 @@ nTwice("Chocolate", 1) → "Ce"
         return str.substring(0, n) + str.substring(str.length() - n);
     }
 
+    /*
+    Given a string, return true if "bad" appears starting at
+    index 0 or 1 in the string, such as with "badxxx" or "xbadxx"
+    but not "xxbadxx". The string may be any length, including 0. Note: use .equals() to compare 2 strings.
+
+
+hasBad("badxx") → true
+hasBad("xbadxx") → true
+hasBad("xxbadxx") → false
+     */
+    public boolean hasBad(String str) {
+        if (str.length() < 3) return false;
+        if (str.equals("bad")) return true;
+        if (str.length() == 3) return false;
+        return str.substring(0,3).equals("bad") || str.substring(1,4).equals("bad");
+    }
+
 
 }
