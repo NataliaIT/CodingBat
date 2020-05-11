41b869222e5eee1fe53d3cc4e91d0c91337efbc6
Natalia <natikrak@gmail.com>
2020-05-11 16:49:16 +0200

    extraEnd

--- src/main/java/string/String1.java
+++ src/main/java/string/String1.java
@@ -237,4 +237,20 @@ makeAbba("What", "Up") → "WhatUpUpWhat"
     public String makeAbba(String a, String b) {
 return a+b+b+a;
     }
+
+/*
+Given a string, return a new string made of 3 copies
+of the last 2 chars of the original string. The string length will be at least 2.
+
+
+extraEnd("Hello") → "lololo"
+extraEnd("ab") → "ababab"
+extraEnd("Hi") → "HiHiHi"
+ */
+
+
+    public String extraEnd(String str) {
+        String end = str.substring(str.length()-2);
+        return end+end+end;
+    }
 }
