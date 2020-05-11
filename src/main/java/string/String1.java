be039984b449c5524b7dfd4fe6e7dee8aaf47978
Natalia <natikrak@gmail.com>
2020-05-11 16:44:31 +0200

    startWord

--- src/main/java/string/String1.java
+++ src/main/java/string/String1.java
@@ -193,4 +193,31 @@ extraFront("H") → "HHH"
         return newStr;
     }
 
+
+    /*
+    Given a string and a second "word" string, we'll say
+    that the word matches the string if it appears at the front
+     of the string, except its first char does not need to match exactly.
+     On a match, return the front of the string, or otherwise
+     return the empty string. So, so with the string "hippo" the word "hi"
+     returns "hi" and "xip" returns "hip". The word will be at least length 1.
+
+
+startWord("hippo", "hi") → "hi"
+startWord("hippo", "xip") → "hip"
+startWord("hippo", "i") → "h"
+     */
+    public String startWord(String str, String word) {
+        int strLength = str.length();
+        int wordLength = word.length();
+        String resultStr = "";
+        if (strLength >= wordLength) {
+
+            if (str.substring(1, wordLength).equals(word.substring(1))) {
+                resultStr = str.charAt(0) + word.substring(1);
+            }
+
+        }
+        return resultStr;
+    }
 }
