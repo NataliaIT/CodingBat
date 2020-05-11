28e7c5c466a344be093fe0312d0ac7072d3ddd20
Natalia <natikrak@gmail.com>
2020-05-11 16:46:36 +0200

    makeAbba

--- src/main/java/string/String1.java
+++ src/main/java/string/String1.java
@@ -220,4 +220,21 @@ startWord("hippo", "i") → "h"
         }
         return resultStr;
     }
+
+
+    /*
+
+Given two strings, a and b, return the result of putting
+them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+
+makeAbba("Hi", "Bye") → "HiByeByeHi"
+makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+makeAbba("What", "Up") → "WhatUpUpWhat"
+     */
+
+
+    public String makeAbba(String a, String b) {
+return a+b+b+a;
+    }
 }
