56d2f43c7d68119866c12258b38d86d1db057933
Natalia <natikrak@gmail.com>
2020-05-15 16:57:33 +0200

    commonEnd

--- src/main/java/array/Array1.java
+++ src/main/java/array/Array1.java
@@ -13,6 +13,21 @@ firstLast6([13, 6, 1, 2, 3]) → false
  */
 
     public boolean firstLast6(int[] nums) {
+        return (nums[0] == 6) || (nums[nums.length - 1] == 6);
+    }
+
+    /*
+
+Given 2 arrays of ints, a and b, return true if they have the same
+first element or they have the same last element. Both arrays will be length 1 or more.
+
+
+commonEnd([1, 2, 3], [7, 3]) → true
+commonEnd([1, 2, 3], [7, 3, 2]) → false
+commonEnd([1, 2, 3], [1, 3]) → true
+     */
+
+    public boolean commonEnd(int[] a, int[] b) {
 
     }
 
