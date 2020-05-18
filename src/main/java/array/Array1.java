85c948fd7e10e10abe57053280db86125f5657f4
Natalia <natikrak@gmail.com>
2020-05-18 19:29:27 +0200

    revers3

--- src/main/java/array/Array1.java
+++ src/main/java/array/Array1.java
@@ -28,7 +28,24 @@ commonEnd([1, 2, 3], [1, 3]) → true
      */
 
     public boolean commonEnd(int[] a, int[] b) {
-        return a[0]==b[0] || a[a.length-1]==b[b.length-1];
+        return a[0] == b[0] || a[a.length - 1] == b[b.length - 1];
+    }
+
+
+    /*
+
+Given an array of ints length 3, return a new array with
+the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+
+
+reverse3([1, 2, 3]) → [3, 2, 1]
+reverse3([5, 11, 9]) → [9, 11, 5]
+reverse3([7, 0, 0]) → [0, 0, 7]
+     */
+    public int[] reverse3(int[] nums) {
+        int[] newNums = new int[]{nums[2], nums[1], nums[0]};
+
+        return newNums;
     }
 
 
