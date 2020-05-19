55ac420d612efcc4dcea5600f931f3d150af94be
Natalia <natikrak@gmail.com>
2020-05-19 11:55:27 +0200

    midThree

--- src/main/java/array/Array1.java
+++ src/main/java/array/Array1.java
@@ -126,5 +126,22 @@ makeMiddle([1, 2]) → [1, 2]
         return resultNums;
     }
 
+    /*
+
+Given an array of ints of odd length, return a new array length 3
+containing the elements from the middle of the array. The array length will be at least 3.
+
+
+midThree([1, 2, 3, 4, 5]) → [2, 3, 4]
+midThree([8, 6, 7, 5, 3, 0, 9]) → [7, 5, 3]
+midThree([1, 2, 3]) → [1, 2, 3]
+     */
+
+    public int[] midThree(int[] nums) {
+        int middle = nums.length / 2;
+
+        int[] resultArray = new int[]{nums[middle - 1], nums[middle], nums[middle + 1]};
+        return resultArray;
+    }
 
 }
