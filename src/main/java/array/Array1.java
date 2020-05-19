d7ae8b845d6634431dcfb6455badf67f06764153
Natalia <natikrak@gmail.com>
2020-05-19 12:26:20 +0200

    makeEnds

--- src/main/java/array/Array1.java
+++ src/main/java/array/Array1.java
@@ -214,13 +214,27 @@ maxEnd3([2, 11, 3]) → [3, 3, 3]
 
 
     public int[] maxEnd3(int[] nums) {
-        int max = nums[0];
-        if (nums[0] < nums[nums.length - 1]) {
-            max = nums[nums.length - 1];
-        }
-
+        int max = nums[0] > nums[nums.length - 1] ? nums[0] : nums[nums.length - 1];
         int[] maxArray = new int[]{max, max, max};
         return maxArray;
+    }
+
+
+    /*
+
+Given an array of ints, return a new array length 2 containing the first
+and last elements from the original array. The original array will be length 1 or more.
+
+
+makeEnds([1, 2, 3]) → [1, 3]
+makeEnds([1, 2, 3, 4]) → [1, 4]
+makeEnds([7, 4, 6, 2]) → [7, 2]
+     */
+
+
+    public int[] makeEnds(int[] nums) {
+        int[] resultArray = new int[]{nums[0], nums[nums.length - 1]};
+        return resultArray;
     }
 
 
