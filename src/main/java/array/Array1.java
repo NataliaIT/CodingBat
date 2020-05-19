58922bdf7ea43c77be3f75e42aaa78d1fa744f05
Natalia <natikrak@gmail.com>
2020-05-19 12:18:55 +0200

    maxEnd3

--- src/main/java/array/Array1.java
+++ src/main/java/array/Array1.java
@@ -199,4 +199,29 @@ sum3([7, 0, 0]) → 7
         return nums[0] + nums[1] + nums[2];
     }
 
+
+    /*
+
+Given an array of ints length 3, figure out which is larger, the first or
+last element in the array, and set all the other elements to be that value.
+Return the changed array.
+
+
+maxEnd3([1, 2, 3]) → [3, 3, 3]
+maxEnd3([11, 5, 9]) → [11, 11, 11]
+maxEnd3([2, 11, 3]) → [3, 3, 3]
+     */
+
+
+    public int[] maxEnd3(int[] nums) {
+        int max = nums[0];
+        if (nums[0] < nums[nums.length - 1]) {
+            max = nums[nums.length - 1];
+        }
+
+        int[] maxArray = new int[]{max, max, max};
+        return maxArray;
+    }
+
+
 }
