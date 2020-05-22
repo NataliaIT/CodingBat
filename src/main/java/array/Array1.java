ced2575887a772c8b8c003f7c00280834cebd6bf
Natalia <natikrak@gmail.com>
2020-05-22 17:27:56 +0200

    has23

--- src/main/java/array/Array1.java
+++ src/main/java/array/Array1.java
@@ -430,4 +430,28 @@ sum2([1, 1, 1, 1]) → 2
         }
         return sum;
     }
+
+    /*
+    Given an int array length 2, return true if it contains a 2 or a 3.
+
+
+has23([2, 5]) → true
+has23([4, 3]) → true
+has23([4, 5]) → false
+     */
+
+
+    public boolean has23(int[] nums) {
+
+        for (int n : nums) {
+            if (n==2) {
+                return true;
+            }
+            else if (n==3){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
