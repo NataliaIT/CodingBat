409ef2d2fe8d4b2978e6932ad0d64f5ec9510978
Natalia <natikrak@gmail.com>
2020-05-22 17:12:57 +0200

    rotateLeft3

--- src/main/java/array/Array1.java
+++ src/main/java/array/Array1.java
@@ -363,7 +363,7 @@ make2([], [1, 2]) → [1, 2]
         }
 
         i = 0;
-        while (count < 2 && i< b.length) {
+        while (count < 2 && i < b.length) {
             resultArray[count] = b[i];
             count++;
             i++;
@@ -380,8 +380,29 @@ makePi() → [3, 1, 4]
      */
 
     public int[] makePi() {
-        int[] arrayPi = new int[] {3,1,4};
-return arrayPi;
+        int[] arrayPi = new int[]{3, 1, 4};
+        return arrayPi;
+    }
+
+    /*
+
+Given an array of ints length 3, return an array with the elements
+"rotated left" so {1, 2, 3} yields {2, 3, 1}.
+
+
+rotateLeft3([1, 2, 3]) → [2, 3, 1]
+rotateLeft3([5, 11, 9]) → [11, 9, 5]
+rotateLeft3([7, 0, 0]) → [0, 0, 7]
+     */
+
+
+    public int[] rotateLeft3(int[] nums) {
+        int temp = nums[0];
+        nums[0] = nums[1];
+        nums[1] = nums[2];
+        nums[2] = temp;
+
+        return nums;
     }
 
 
