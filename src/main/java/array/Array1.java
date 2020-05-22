a27b5c2d2886761088e5186802987168f0480c18
Natalia <natikrak@gmail.com>
2020-05-22 17:06:24 +0200

    makePI

--- src/main/java/array/Array1.java
+++ src/main/java/array/Array1.java
@@ -371,4 +371,18 @@ make2([], [1, 2]) → [1, 2]
         return resultArray;
     }
 
+    /*
+
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+
+makePi() → [3, 1, 4]
+     */
+
+    public int[] makePi() {
+        int[] arrayPi = new int[] {3,1,4};
+return arrayPi;
+    }
+
+
 }
