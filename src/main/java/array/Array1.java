4e07a5d18a1146a44d0d50b5f31bc7a56906fe68
Natalia <natikrak@gmail.com>
2020-05-23 14:44:21 +0200

    double23

--- src/main/java/array/Array1.java
+++ src/main/java/array/Array1.java
@@ -17,10 +17,8 @@ firstLast6([13, 6, 1, 2, 3]) → false
     }
 
     /*
-
 Given 2 arrays of ints, a and b, return true if they have the same
 first element or they have the same last element. Both arrays will be length 1 or more.
-
 
 commonEnd([1, 2, 3], [7, 3]) → true
 commonEnd([1, 2, 3], [7, 3, 2]) → false
@@ -33,27 +31,23 @@ commonEnd([1, 2, 3], [1, 3]) → true
 
 
     /*
-
 Given an array of ints length 3, return a new array with
 the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
-
 
 reverse3([1, 2, 3]) → [3, 2, 1]
 reverse3([5, 11, 9]) → [9, 11, 5]
 reverse3([7, 0, 0]) → [0, 0, 7]
      */
+
     public int[] reverse3(int[] nums) {
         int[] newNums = new int[]{nums[2], nums[1], nums[0]};
-
         return newNums;
     }
 
 
     /*
-
 Given 2 int arrays, a and b, each length 3,
 return a new array length 2 containing their middle elements.
-
 
 middleWay([1, 2, 3], [4, 5, 6]) → [2, 5]
 middleWay([7, 7, 7], [3, 8, 0]) → [7, 8]
@@ -66,16 +60,13 @@ middleWay([5, 2, 9], [1, 4, 5]) → [2, 4]
         return result;
     }
 
-
     /*
-    Given an int array length 2, return true if it does not contain a 2 or 3.
-
+Given an int array length 2, return true if it does not contain a 2 or 3.
 
 no23([4, 5]) → true
 no23([4, 2]) → false
 no23([3, 5]) → false
      */
-
 
     public boolean no23(int[] nums) {
 
@@ -87,10 +78,8 @@ no23([3, 5]) → false
     }
 
     /*
-
 Given an int array length 3, if there is a 2 in the array immediately
 followed by a 3, set the 3 element to 0. Return the changed array.
-
 
 fix23([1, 2, 3]) → [1, 2, 0]
 fix23([2, 3, 5]) → [2, 0, 5]
@@ -108,11 +97,9 @@ fix23([1, 2, 1]) → [1, 2, 1]
 
 
     /*
-
 Given an array of ints of even length, return a new array length 2
 containing the middle two elements from the original array.
 The original array will be length 2 or more.
-
 
 makeMiddle([1, 2, 3, 4]) → [2, 3]
 makeMiddle([7, 1, 2, 3, 4, 9]) → [2, 3]
@@ -127,10 +114,8 @@ makeMiddle([1, 2]) → [1, 2]
     }
 
     /*
-
 Given an array of ints of odd length, return a new array length 3
 containing the elements from the middle of the array. The array length will be at least 3.
-
 
 midThree([1, 2, 3, 4, 5]) → [2, 3, 4]
 midThree([8, 6, 7, 5, 3, 0, 9]) → [7, 5, 3]
@@ -139,22 +124,20 @@ midThree([1, 2, 3]) → [1, 2, 3]
 
     public int[] midThree(int[] nums) {
         int middle = nums.length / 2;
-
         int[] resultArray = new int[]{nums[middle - 1], nums[middle], nums[middle + 1]};
         return resultArray;
     }
 
 
     /*
-
 We'll say that a 1 immediately followed by a 3 in an array is an "unlucky" 1.
 Return true if the given array contains an unlucky 1 in the first 2 or last 2 positions in the array.
-
 
 unlucky1([1, 3, 4, 5]) → true
 unlucky1([2, 1, 3, 4, 5]) → true
 unlucky1([1, 1, 1]) → false
      */
+
     public boolean unlucky1(int[] nums) {
         if (nums.length > 1) {
             for (int i = 0; i < 2; i++) {
@@ -170,10 +153,8 @@ unlucky1([1, 1, 1]) → false
 
 
     /*
-
 Given an array of ints, return true if the array is length 1 or more,
 and the first element and the last element are equal.
-
 
 sameFirstLast([1, 2, 3]) → false
 sameFirstLast([1, 2, 3, 1]) → true
@@ -181,14 +162,13 @@ sameFirstLast([1, 2, 1]) → true
      */
 
     public boolean sameFirstLast(int[] nums) {
+
         return nums.length > 0 && nums[0] == nums[nums.length - 1];
     }
 
 
     /*
-
 Given an array of ints length 3, return the sum of all the elements.
-
 
 sum3([1, 2, 3]) → 6
 sum3([5, 11, 2]) → 18
@@ -196,16 +176,15 @@ sum3([7, 0, 0]) → 7
      */
 
     public int sum3(int[] nums) {
+
         return nums[0] + nums[1] + nums[2];
     }
 
 
     /*
-
 Given an array of ints length 3, figure out which is larger, the first or
 last element in the array, and set all the other elements to be that value.
 Return the changed array.
-
 
 maxEnd3([1, 2, 3]) → [3, 3, 3]
 maxEnd3([11, 5, 9]) → [11, 11, 11]
@@ -219,12 +198,9 @@ maxEnd3([2, 11, 3]) → [3, 3, 3]
         return maxArray;
     }
 
-
     /*
-
 Given an array of ints, return a new array length 2 containing the first
 and last elements from the original array. The original array will be length 1 or more.
-
 
 makeEnds([1, 2, 3]) → [1, 3]
 makeEnds([1, 2, 3, 4]) → [1, 4]
@@ -238,19 +214,18 @@ makeEnds([7, 4, 6, 2]) → [7, 2]
     }
 
     /*
-    Given an int array, return a new array with double the length where
-    its last element is the same as the original array, and all the other
-    elements are 0. The original array will be length 1 or more.
-    Note: by default, a new int array contains all 0's.
-
+Given an int array, return a new array with double the length where
+its last element is the same as the original array, and all the other
+elements are 0. The original array will be length 1 or more.
+Note: by default, a new int array contains all 0's.
 
 makeLast([4, 5, 6]) → [0, 0, 0, 0, 0, 6]
 makeLast([1, 2]) → [0, 0, 0, 2]
 makeLast([3]) → [0, 3]
      */
 
-
     public int[] makeLast(int[] nums) {
+
         int[] resultArray = new int[nums.length * 2];
 
         for (int i = 0; i < resultArray.length - 2; i++) {
@@ -261,10 +236,8 @@ makeLast([3]) → [0, 3]
     }
 
     /*
-
 Start with 2 int arrays, a and b, of any length.
 Return how many of the arrays have 1 as their first element.
-
 
 start1([1, 2, 3], [1, 3]) → 2
 start1([7, 2, 3], [1]) → 1
@@ -283,33 +256,27 @@ start1([1, 2], []) → 1
     }
 
     /*
-
 Given 2 int arrays, each length 2, return a new array
 length 4 containing all their elements.
-
 
 plusTwo([1, 2], [3, 4]) → [1, 2, 3, 4]
 plusTwo([4, 4], [2, 2]) → [4, 4, 2, 2]
 plusTwo([9, 2], [3, 4]) → [9, 2, 3, 4]
      */
-    public int[] plusTwo(int[] a, int[] b) {
 
+    public int[] plusTwo(int[] a, int[] b) {
         return new int[]{a[0], a[1], b[0], b[1]};
     }
 
-
     /*
-
 Given an array of ints of odd length, look at the first,
 last, and middle values in the array and return the largest.
 The array length will be a least 1.
-
 
 maxTriple([1, 2, 3]) → 3
 maxTriple([1, 5, 3]) → 5
 maxTriple([5, 2, 3]) → 5
      */
-
 
     public int maxTriple(int[] nums) {
         int firstValue = nums[0];
@@ -321,12 +288,10 @@ maxTriple([5, 2, 3]) → 5
 
 
 /*
-
 Given 2 int arrays, a and b, return a new array length 2 containing,
 as much as will fit, the elements from a followed by the elements from b.
 The arrays may be any length, including 0, but there will be 2 or more elements
 available between the 2 arrays.
-
 
 make2([4, 5], [1, 2, 3]) → [4, 5]
 make2([4], [1, 2, 3]) → [4, 1]
@@ -372,10 +337,7 @@ make2([], [1, 2]) → [1, 2]
     }
 
     /*
-
 Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
-
-
 makePi() → [3, 1, 4]
      */
 
@@ -385,16 +347,13 @@ makePi() → [3, 1, 4]
     }
 
     /*
-
 Given an array of ints length 3, return an array with the elements
 "rotated left" so {1, 2, 3} yields {2, 3, 1}.
-
 
 rotateLeft3([1, 2, 3]) → [2, 3, 1]
 rotateLeft3([5, 11, 9]) → [11, 9, 5]
 rotateLeft3([7, 0, 0]) → [0, 0, 7]
      */
-
 
     public int[] rotateLeft3(int[] nums) {
         int temp = nums[0];
@@ -410,16 +369,15 @@ rotateLeft3([7, 0, 0]) → [0, 0, 7]
     }
 
     /*
-
 Given an array of ints, return the sum of the first 2 elements in the array.
 If the array length is less than 2, just sum up the elements that exist,
 returning 0 if the array is length 0.
-
 
 sum2([1, 2, 3]) → 3
 sum2([1, 1]) → 2
 sum2([1, 1, 1, 1]) → 2
      */
+
     public int sum2(int[] nums) {
 
         int sum = 0;
@@ -433,7 +391,6 @@ sum2([1, 1, 1, 1]) → 2
 
     /*
     Given an int array length 2, return true if it contains a 2 or a 3.
-
 
 has23([2, 5]) → true
 has23([4, 3]) → true
@@ -454,10 +411,8 @@ has23([4, 5]) → false
     }
 
     /*
-
 Given an int array, return true if the array contains 2 twice, or 3 twice.
 The array will be length 0, 1, or 2.
-
 
 double23([2, 2]) → true
 double23([3, 3]) → true
@@ -471,5 +426,21 @@ double23([2, 3]) → false
             }
         }
         return false;
+    }
+
+    public boolean double23SecondSolution(int[] nums) {
+
+        int count2 = 0;
+        int count3 = 0;
+
+        for (int num : nums) {
+            if (num == 2) {
+                count2++;
+            } else if (num == 3) {
+                count3++;
+            }
+        }
+
+        return count2 == 2 || count3 == 2;
     }
 }
