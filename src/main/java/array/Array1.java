f489b6de21e53060ffea41e703353c0cc1ec050b
Natalia <natikrak@gmail.com>
2020-05-23 15:22:46 +0200

    front11

--- src/main/java/array/Array1.java
+++ src/main/java/array/Array1.java
@@ -508,4 +508,32 @@ frontPiece([1]) → [1]
         }
         return front;
     }
+
+    /*
+Given 2 int arrays, a and b, of any length, return a new array
+with the first element of each array. If either array is length 0,
+ignore that array.
+
+front11([1, 2, 3], [7, 9, 8]) → [1, 7]
+front11([1], [2]) → [1, 2]
+front11([1, 7], []) → [1]
+     */
+
+    public int[] front11(int[] a, int[] b) {
+        int[] front11;
+        if (a.length > 0 && b.length > 0) {
+            front11 = new int[2];
+            front11[0] = a[0];
+            front11[1] = b[0];
+        } else if (a.length > b.length) {
+            front11 = new int[1];
+            front11[0] = a[0];
+        } else if (a.length < b.length) {
+            front11 = new int[1];
+            front11[0] = b[0];
+        } else {
+            front11 = new int[0];
+        }
+        return front11;
+    }
 }
