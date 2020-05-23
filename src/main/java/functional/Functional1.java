51d0ecbc2a47adb8f1160b35c50bcba805df085d
Natalia <natikrak@gmail.com>
2020-05-23 16:16:49 +0200

    addStar

--- src/main/java/functional/Functional1.java
+++ src/main/java/functional/Functional1.java
@@ -72,4 +72,31 @@ moreY(["yay"]) → ["yyayy"]
     }
 
 
+    /*
+ Given a list of strings, return a list where each string is converted to lower case (Note: String toLowerCase() method).
+
+lower(["Hello", "Hi"]) → ["hello", "hi"]
+lower(["AAA", "BBB", "ccc"]) → ["aaa", "bbb", "ccc"]
+lower(["KitteN", "ChocolaTE"]) → ["kitten", "chocolate"]
+     */
+
+    public List<String> lower(List<String> strings) {
+        strings.replaceAll(n -> n.toLowerCase());
+        return strings;
+    }
+
+    /*
+    Given a list of strings, return a list where each string has "*" added at its end.
+addStar(["a", "bb", "ccc"]) → ["a*", "bb*", "ccc*"]
+addStar(["hello", "there"]) → ["hello*", "there*"]
+addStar(["*"]) → ["**"]
+     */
+
+    public List<String> addStar(List<String> strings) {
+        strings.replaceAll(n -> n + "*");
+        return strings;
+    }
+
+
+
 }
