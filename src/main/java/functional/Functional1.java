7dc6ed12403ac2a4dd17b850a0792cdade480796
Natalia <natikrak@gmail.com>
2020-05-23 16:11:19 +0200

    square

--- src/main/java/functional/Functional1.java
+++ src/main/java/functional/Functional1.java
@@ -46,5 +46,17 @@ rightDigit([10, 0]) → [0, 0]
                 .collect(Collectors.toList());
     }
 
+    /*
+Given a list of integers, return a list where each integer is multiplied with itself.
+
+square([1, 2, 3]) → [1, 4, 9]
+square([6, 8, -6, -8, 1]) → [36, 64, 36, 64, 1]
+square([]) → []
+     */
+    public List<Integer> square(List<Integer> nums) {
+        nums.replaceAll(n -> n * n);
+        return nums;
+    }
+
 
 }
