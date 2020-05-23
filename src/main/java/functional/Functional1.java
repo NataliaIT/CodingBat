3daff099fa6d254197447948dbe4ea4e2c508281
Natalia <natikrak@gmail.com>
2020-05-23 16:09:07 +0200

    rightDigit

--- src/main/java/functional/Functional1.java
+++ src/main/java/functional/Functional1.java
@@ -33,5 +33,18 @@ copies3(["hello", "there"]) → ["hellohellohello", "theretherethere"]
                 .collect(Collectors.toList());
     }
 
+    /*
+Given a list of non-negative integers, return an integer list of the rightmost digits. (Note: use %)
+
+rightDigit([1, 22, 93]) → [1, 2, 3]
+rightDigit([16, 8, 886, 8, 1]) → [6, 8, 6, 8, 1]
+rightDigit([10, 0]) → [0, 0]
+     */
+    public List<Integer> rightDigit(List<Integer> nums) {
+        return nums.stream()
+                .map(n -> n % 10)
+                .collect(Collectors.toList());
+    }
+
 
 }
