8aa791a2068583fddae753b20819f216691ae3a1
Natalia <natikrak@gmail.com>
2020-05-23 15:40:12 +0200

    copies3

--- src/main/java/functional/Functional1.java
+++ src/main/java/functional/Functional1.java
@@ -1,6 +1,7 @@
 package functional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Functional1 {
 /*
@@ -25,5 +26,12 @@ copies3(["a", "bb", "ccc"]) → ["aaa", "bbbbbb", "ccccccccc"]
 copies3(["24", "a", ""]) → ["242424", "aaa", ""]
 copies3(["hello", "there"]) → ["hellohellohello", "theretherethere"]
      */
+
+    public List<String> copies3(List<String> strings) {
+        return strings.stream()
+                .map(n -> n + n + n)
+                .collect(Collectors.toList());
+    }
+
 
 }
