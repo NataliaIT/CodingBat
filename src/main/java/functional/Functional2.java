836f6f38409510974f97c1bce5344b3449a4477a
Natalia <natikrak@gmail.com>
2020-05-23 16:54:09 +0200

    noZ

--- src/main/java/functional/Functional2.java
+++ src/main/java/functional/Functional2.java
@@ -12,8 +12,23 @@ noNeg([-3, -3, 3, 3]) → [3, 3]
 noNeg([-1, -1, -1]) → []
      */
     public List<Integer> noNeg(List<Integer> nums) {
-nums.removeIf(n->n<0);
-return nums;
+        nums.removeIf(n -> n < 0);
+        return nums;
     }
 
+
+    /*
+Given a list of strings, return a list of the strings,
+omitting any string that contains a "z".
+(Note: the str.contains(x) method returns a boolean)
+
+noZ(["aaa", "bbb", "aza"]) → ["aaa", "bbb"]
+noZ(["hziz", "hzello", "hi"]) → ["hi"]
+noZ(["hello", "howz", "are", "youz"]) → ["hello", "are"]
+     */
+
+    public List<String> noZ(List<String> strings) {
+        strings.removeIf(n -> n.contains("z"));
+        return strings;
+    }
 }
