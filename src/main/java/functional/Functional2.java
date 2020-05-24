6f81e7928580392202d8cca2703711e1a8f3c8b5
Natalia <natikrak@gmail.com>
2020-05-24 19:36:08 +0200

    noLong

--- src/main/java/functional/Functional2.java
+++ src/main/java/functional/Functional2.java
@@ -63,4 +63,16 @@ no9([1, 2, 3]) → [1, 2, 3]
         return nums.stream().filter(n -> !(n % 10 == 9)).collect(Collectors.toList());
     }
 
+
+    /*
+Given a list of strings, return a list of the strings, omitting any string length 4 or more.
+
+noLong(["this", "not", "too", "long"]) → ["not", "too"]
+noLong(["a", "bbb", "cccc"]) → ["a", "bbb"]
+noLong(["cccc", "cccc", "cccc"]) → []
+     */
+
+    public List<String> noLong(List<String> strings) {
+        return strings.stream().filter(n->n.length()<4).collect(Collectors.toList());
+    }
 }
