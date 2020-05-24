637896c0ced79381dec4c725f906be8bb6b74a5f
Natalia <natikrak@gmail.com>
2020-05-24 20:35:04 +0200

    mapBully

--- src/main/java/logic/Logic1.java
+++ src/main/java/logic/Logic1.java
@@ -13,7 +13,14 @@ cigarParty(70, true) → true
 
 
     public boolean cigarParty(int cigars, boolean isWeekend) {
-
+        if (isWeekend) {
+            if(cigars>=40)
+                return true;
+        } else {
+            if(cigars>=40 && cigars<=60)
+                return true;
+        }
+        return false;
     }
 
 }
