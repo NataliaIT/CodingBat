583d298e28051c6102791d517cc829f234054b97
Natalia <natikrak@gmail.com>
2020-05-26 23:29:52 +0200

    countCode

--- src/main/java/string/String2.java
+++ src/main/java/string/String2.java
@@ -18,6 +18,27 @@ doubleChar("Hi-There") → "HHii--TThheerree"
             result = result + str.charAt(i) + str.charAt(i);
         }
         return result;
+    }
 
+    /*
+Return the number of times that the string "code" appears anywhere in the given string,
+except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+
+countCode("aaacodebbb") → 1
+countCode("codexxcode") → 2
+countCode("cozexxcope") → 2
+     */
+
+
+    public int countCode(String str) {
+        int count = 0;
+        if (str.length() > 3)
+            for (int i = 0; i < str.length() - 3; i++) {
+                if (str.charAt(i) == 'c' && str.charAt(i + 1) == 'o' && str.charAt(i + 3) == 'e') {
+                    count++;
+                }
+            }
+        return count;
     }
 }
