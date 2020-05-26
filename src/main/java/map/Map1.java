fefb1317f50c62cd7b9558f4f7fc205514633948
Natalia <natikrak@gmail.com>
2020-05-26 22:42:45 +0200

    topping3

--- src/main/java/map/Map1.java
+++ src/main/java/map/Map1.java
@@ -46,7 +46,6 @@ topping1({"pancake": "syrup"}) → {"bread": "butter", "pancake": "syrup"}
     /*
 Modify and return the given map as follows: if the keys "a" and "b" are both in the map and have equal values, remove them both.
 
-
 mapAB2({"a": "aaa", "b": "aaa", "c": "cake"}) → {"c": "cake"}
 mapAB2({"a": "aaa", "b": "bbb"}) → {"a": "aaa", "b": "bbb"}
 mapAB2({"a": "aaa", "b": "bbb", "c": "aaa"}) → {"a": "aaa", "b": "bbb", "c": "aaa"}
@@ -131,12 +130,34 @@ mapAB({"b": "There"}) → {"b": "There"}
      */
 
     public Map<String, String> mapAB(Map<String, String> map) {
-        if(map.containsKey("a") && map.containsKey("b")){
-            map.put("ab", (map.get("a")+map.get("b")));
+        if (map.containsKey("a") && map.containsKey("b")) {
+            map.put("ab", (map.get("a") + map.get("b")));
         }
         return map;
     }
 
+    /*
+
+Given a map of food keys and topping values,
+modify and return the map as follows:
+if the key "potato" has a value, set that as the value for the key "fries".
+If the key "salad" has a value, set that as the value for the key "spinach".
+
+
+topping3({"potato": "ketchup"}) → {"potato": "ketchup", "fries": "ketchup"}
+topping3({"potato": "butter"}) → {"potato": "butter", "fries": "butter"}
+topping3({"salad": "oil", "potato": "ketchup"}) → {"spinach": "oil", "salad": "oil", "potato": "ketchup", "fries": "ketchup"}
+     */
+
+    public Map<String, String> topping3(Map<String, String> map) {
+        if (map.containsKey("potato") && map.get("potato") != null) {
+            map.put("fries", map.get("potato"));
+        }
+        if (map.containsKey("salad") && map.get("salad") != null) {
+            map.put("spinach", map.get("salad"));
+        }
+        return map;
+    }
 
 
 }
