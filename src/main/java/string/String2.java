c1434466bc184ce7b6942730c86632548f8aed33
Natalia <natikrak@gmail.com>
2020-05-26 23:35:21 +0200

    bobThere

--- src/main/java/string/String2.java
+++ src/main/java/string/String2.java
@@ -41,4 +41,25 @@ countCode("cozexxcope") → 2
             }
         return count;
     }
+
+    /*
+Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char.
+
+
+bobThere("abcbob") → true
+bobThere("b9b") → true
+bobThere("bac") → false
+     */
+
+    public boolean bobThere(String str) {
+        if (str.length() > 2) {
+            for (int i = 0; i < str.length() - 2; i++) {
+                if (str.charAt(i) == 'b' && str.charAt(i + 2) == 'b') {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
