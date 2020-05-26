415f94afdfc006ef6491292f7bb64215bf3991e0
Natalia <natikrak@gmail.com>
2020-05-26 22:55:12 +0200

    mapAB4

--- src/main/java/map/Map1.java
+++ src/main/java/map/Map1.java
@@ -160,5 +160,33 @@ topping3({"salad": "oil", "potato": "ketchup"}) → {"spinach": "oil", "salad": 
     }
 
 
+
+    /*
+Modify and return the given map as follows:
+if the keys "a" and "b" have values that have different lengths,
+then set "c" to have the longer value.
+If the values exist and have the same length,
+change them both to the empty string in the map.
+
+
+mapAB4({"a": "aaa", "b": "bb", "c": "cake"}) → {"a": "aaa", "b": "bb", "c": "aaa"}
+mapAB4({"a": "aa", "b": "bbb", "c": "cake"}) → {"a": "aa", "b": "bbb", "c": "bbb"}
+mapAB4({"a": "aa", "b": "bbb"}) → {"a": "aa", "b": "bbb", "c": "bbb"}
+     */
+
+    public Map<String, String> mapAB4(Map<String, String> map) {
+        if (map.containsKey("a") && map.containsKey("b")
+                && map.get("a").length() != map.get("b").length()) {
+            String longerValue = map.get("a").length() > map.get("b").length() ? map.get("a") : map.get("b");
+            map.put("c", longerValue);
+        }
+        if (map.containsKey("a") && map.containsKey("b")
+                && map.get("a").length() == map.get("b").length()) {
+            map.put("a", "");
+            map.put("b", "");
+        }
+        return map;
+    }
+
 }
 
