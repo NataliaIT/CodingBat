be60ceccf759efd9af7a3ce5a95c10cf8eb4abe1
Natalia <natikrak@gmail.com>
2020-05-27 23:19:55 +0200

    starOut

--- src/main/java/string/String2.java
+++ src/main/java/string/String2.java
@@ -126,4 +126,17 @@ sameStarChar("*xa*az") → true
         return true;
     }
 
+
+    /*
+Return a version of the given string, where for every star (*)
+in the string the star and the chars immediately to its left and right are gone.
+So "ab*cd" yields "ad" and "ab**cd" also yields "ad".
+
+starOut("ab*cd") → "ad"
+starOut("ab**cd") → "ad"
+starOut("sm*eilly") → "silly"
+     */
+    public String starOut(String str) {
+        String pattern = "([^*]{0,1})\\*+([^*]{0,1})";
+        return str.replaceAll(pattern, "");
 }
