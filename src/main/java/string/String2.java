03d39696a7cd5aa00133e2abae2a1555679fb19a
Natalia <natikrak@gmail.com>
2020-06-05 22:23:55 +0200

    repeatFront

--- src/main/java/string/String2.java
+++ src/main/java/string/String2.java
@@ -208,4 +208,26 @@ xyBalance("yaaxbb") → false
         }
         return true;
     }
-}
+
+
+    /*
+
+Given a string and an int n, return a string made of the first n characters of the string, followed by the first n-1 characters of the string, and so on. You may assume that n is between 0 and the length of the string, inclusive (i.e. n >= 0 and n <= str.length()).
+
+
+repeatFront("Chocolate", 4) → "ChocChoChC"
+repeatFront("Chocolate", 3) → "ChoChC"
+repeatFront("Ice Cream", 2) → "IcI"
+     */
+
+
+    public String repeatFront(String str, int n) {
+        String result = str.substring(0, n);
+
+        if (n > 1) {
+            for (int i = 1; i < n; i++) {
+                result = result + str.substring(0, n - i);
+            }
+        }
+        return result;
+    }
