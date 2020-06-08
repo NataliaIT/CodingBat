244ee0b2020226869c3f93e3944687661ef71743
Natalia <natikrak@gmail.com>
2020-06-08 17:39:13 +0200

    getSandwich

--- src/main/java/string/String2.java
+++ src/main/java/string/String2.java
@@ -371,4 +371,43 @@ mixString("xxxx", "There") → "xTxhxexre"
         return result;
     }
 
+
+    /*
+
+Given two strings, word and a separator sep, return a big string
+made of count occurrences of the word, separated by the separator string.
+
+repeatSeparator("Word", "X", 3) → "WordXWordXWord"
+repeatSeparator("This", "And", 2) → "ThisAndThis"
+repeatSeparator("This", "And", 1) → "This"
+     */
+    public String repeatSeparator(String word, String sep, int count) {
+        String result = "";
+        for (int i = 0; i < count; i++) {
+            result += word;
+            if (i < count - 1) {
+                result += sep;
+            }
+        }
+        return result;
+    }
+
+    /*
+A sandwich is two pieces of bread with something in between.
+Return the string that is between the first and last appearance of
+"bread" in the given string, or return the empty string "" if there are not two pieces of bread.
+
+
+getSandwich("breadjambread") → "jam"
+getSandwich("xxbreadjambreadyy") → "jam"
+getSandwich("xxbreadyy") → ""
+     */
+
+    public String getSandwich(String str) {
+        int indexOfFirstBread = str.indexOf("bread");
+        int indexOfLastBread = str.lastIndexOf("bread");
+        return indexOfLastBread>indexOfFirstBread ?str.substring(indexOfFirstBread+5,indexOfLastBread): "";
+    }
+
+
 }
