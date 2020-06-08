ce86fb78319a2162d5fdaed5f6000e04e0ef8705
Natalia <natikrak@gmail.com>
2020-06-08 20:40:16 +0200

    lastDigit

--- src/main/java/logic/Logic1.java
+++ src/main/java/logic/Logic1.java
@@ -137,4 +137,18 @@ twoAsOne(3, 2, 2) → false
     public boolean twoAsOne(int a, int b, int c) {
 return a+b==c || b+c==a || a+c==b;
     }
+
+
+    /*
+    Given three ints, a b c, return true if two or more of them have the same rightmost digit.
+    The ints are non-negative. Note: the % "mod" operator computes the remainder, e.g. 17 % 10 is 7.
+
+lastDigit(23, 19, 13) → true
+lastDigit(23, 19, 12) → false
+lastDigit(23, 19, 3) → true
+     */
+
+    public boolean lastDigit(int a, int b, int c) {
+        return a % 10 == b % 10 || b % 10 == c % 10 || a % 10 == c % 10;
+    }
 }
