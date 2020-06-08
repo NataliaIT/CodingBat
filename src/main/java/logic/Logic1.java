b6928fd2ea41ab7f7a58d1983055d8707096c6c8
Natalia <natikrak@gmail.com>
2020-06-08 20:10:11 +0200

    caughtSpeeding

--- src/main/java/logic/Logic1.java
+++ src/main/java/logic/Logic1.java
@@ -14,13 +14,47 @@ cigarParty(70, true) → true
 
     public boolean cigarParty(int cigars, boolean isWeekend) {
         if (isWeekend) {
-            if(cigars>=40)
+            if (cigars >= 40)
                 return true;
         } else {
-            if(cigars>=40 && cigars<=60)
+            if (cigars >= 40 && cigars <= 60)
                 return true;
         }
         return false;
+    }
+
+
+    /*
+    You are driving a little too fast, and a police officer stops you.
+    Write code to compute the result, encoded as an int value: 0=no ticket,
+    1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0.
+    If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more,
+    the result is 2. Unless it is your birthday -- on that day,
+    your speed can be 5 higher in all cases.
+
+
+caughtSpeeding(60, false) → 0
+caughtSpeeding(65, false) → 1
+caughtSpeeding(65, true) → 0
+     */
+
+    public int caughtSpeeding(int speed, boolean isBirthday) {
+        if (!isBirthday) {
+            if (speed > 60 && speed <= 80) {
+                return 1;
+            } else if (speed >= 81) {
+                return 2;
+            }
+        }
+        if (isBirthday) {
+
+            if (66 <= speed && speed <= 85) {
+                return 1;
+            } else if (86 <= speed) {
+                return 2;
+            }
+        }
+        return 0;
     }
 
 }
