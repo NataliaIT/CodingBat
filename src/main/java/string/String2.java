054ccb2862735c449075a2ee7b0243d90d0d4413
Natalia <natikrak@gmail.com>
2020-06-08 18:04:33 +0200

    wordEnds

--- src/main/java/string/String2.java
+++ src/main/java/string/String2.java
@@ -438,4 +438,27 @@ zipZap("zzzopzop") → "zzzpzp"
     }
 
 
+    /*
+Given a string and a non-empty word string,
+return a string made of each char just before and
+just after every appearance of the word in the string.
+Ignore cases where there is no char before or after the word,
+and a char may be included twice if it is between two words.
+
+
+wordEnds("abcXY123XYijk", "XY") → "c13i"
+wordEnds("XY123XY", "XY") → "13"
+wordEnds("XY1XY", "XY") → "11"
+     */
+
+    public String wordEnds(String str, String word) {
+        return str.replaceAll(
+                ".*?(?=word)(?<=(.|^))word(?=(.|$))|.+"
+                        .replace("word", java.util.regex.Pattern.quote(word)),
+                "$1$2"
+        );
+
+    }
+
+
 }
