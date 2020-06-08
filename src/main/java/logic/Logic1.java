a6db62436c5bf5e6f91294f1beff8ab3fb6b24d6
Natalia <natikrak@gmail.com>
2020-06-08 22:11:33 +0200

    dateFashion

--- src/main/java/logic/Logic1.java
+++ src/main/java/logic/Logic1.java
@@ -188,12 +188,35 @@ blueTicket(6, 1, 4) → 10
      */
 
     public int blueTicket(int a, int b, int c) {
-        public int blueTicket ( int a, int b, int c){
-            if (a + b == 10 || b + c == 10 || c + a == 10) {
-                return 10;
-            } else if ((a + b == 10 + b + c) || (a + b == 10 + a + c)) {
-                return 5;
-            } else return 0;
-        }
+
+        if (a + b == 10 || b + c == 10 || c + a == 10) {
+            return 10;
+        } else if ((a + b == 10 + b + c) || (a + b == 10 + a + c)) {
+            return 5;
+        } else return 0;
+    }
+
+
+    /*
+    You and your date are trying to get a table at a restaurant.
+    The parameter "you" is the stylishness of your clothes, in the range 0..10,
+    and "date" is the stylishness of your date's clothes.
+    The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes.
+    If either of you is very stylish, 8 or more, then the result is 2 (yes).
+    With the exception that if either of you has style of 2 or less, then the result is 0 (no).
+    Otherwise the result is 1 (maybe).
+
+
+dateFashion(5, 10) → 2
+dateFashion(5, 2) → 0
+dateFashion(5, 5) → 1
+     */
+
+    public int dateFashion(int you, int date) {
+        if (you < 3 || date < 3) {
+            return 0;
+        } else if (you > 7 || date > 7) {
+            return 2;
+        } else return 1;
     }
 }
