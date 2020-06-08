fa130dcd1622032de2ae9694228a52028e960dd5
Natalia <natikrak@gmail.com>
2020-06-08 18:00:37 +0200

    zipZap

--- src/main/java/string/String2.java
+++ src/main/java/string/String2.java
@@ -406,7 +406,35 @@ getSandwich("xxbreadyy") → ""
     public String getSandwich(String str) {
         int indexOfFirstBread = str.indexOf("bread");
         int indexOfLastBread = str.lastIndexOf("bread");
-        return indexOfLastBread>indexOfFirstBread ?str.substring(indexOfFirstBread+5,indexOfLastBread): "";
+        return indexOfLastBread > indexOfFirstBread ? str.substring(indexOfFirstBread + 5, indexOfLastBread) : "";
+    }
+
+    /*
+Look for patterns like "zip" and "zap" in the string -- length-3,
+starting with 'z' and ending with 'p'. Return a string where for all such words,
+the middle letter is gone, so "zipXzap" yields "zpXzp".
+
+
+zipZap("zipXzap") → "zpXzp"
+zipZap("zopzop") → "zpzp"
+zipZap("zzzopzop") → "zzzpzp"
+     */
+
+    public String zipZap(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); ) {
+            if (str.length()<3){
+                return str;
+            }
+            else if (str.charAt(i) == 'z' && str.charAt(i + 2) == 'p') {
+                result =result+ str.charAt(i) + str.charAt(i + 2);
+                i += 3;
+            } else {
+                result =result+ str.charAt(i);
+                i++;
+            }
+        }
+        return result;
     }
 
 
