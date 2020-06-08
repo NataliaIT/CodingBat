6c8e3d3d4194ef701d10ed2e8af1f6a7756afbc7
Natalia <natikrak@gmail.com>
2020-06-08 20:18:55 +0200

    more20

--- src/main/java/logic/Logic1.java
+++ src/main/java/logic/Logic1.java
@@ -72,4 +72,15 @@ love6(1, 5) → true
 return a==6 || b==6 || (a+b)==6 || Math.abs(a-b)==6;
     }
 
+
+    /*
+    Return true if the given non-negative number is 1 or 2 more than a multiple of 20. See also: Introduction to Mod
+
+more20(20) → false
+more20(21) → true
+more20(22) → true
+     */
+    public boolean more20(int n) {
+        return n % 20 == 1 || n % 20 == 2;
+    }
 }
