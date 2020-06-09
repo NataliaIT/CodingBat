790c7ac2af78ca0660921d7bfd8a2df0653fdcac
Natalia <natikrak@gmail.com>
2020-06-09 16:07:22 +0200

    old35

--- src/main/java/logic/Logic1.java
+++ src/main/java/logic/Logic1.java
@@ -263,4 +263,21 @@ in1To10(11, true) → true
         return false;
     }
 
+
+    /*
+    Return true if the given non-negative number is a multiple of 3 or 5, but not both.
+
+
+old35(3) → true
+old35(10) → true
+old35(15) → false
+     */
+
+    public boolean old35(int n) {
+        if(n%3==0 && n%5==0){
+            return false;
+        }
+        return n%3==0 || n%5==0;
+    }
+
 }
