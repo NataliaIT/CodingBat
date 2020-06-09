346c1bb716f9a683ac0e1e314b792e67125ee501
Natalia <natikrak@gmail.com>
2020-06-09 21:02:57 +0200

    alarmClock

--- src/main/java/logic/Logic1.java
+++ src/main/java/logic/Logic1.java
@@ -391,7 +391,7 @@ shareDigit(12, 44) → false
      */
     public boolean shareDigit(int a, int b) {
 
-        return a/10==b/10 || a/10==b%10 || a%10==b/10 ||a%10==b%10;
+        return a / 10 == b / 10 || a / 10 == b % 10 || a % 10 == b / 10 || a % 10 == b % 10;
     }
 
     /*
@@ -409,6 +409,33 @@ squirrelPlay(95, true) → true
      */
 
     public boolean squirrelPlay(int temp, boolean isSummer) {
-        return (temp>=60 && temp<=90 && !isSummer) ||(temp>=60 && temp<=100 && isSummer);
+        return (temp >= 60 && temp <= 90 && !isSummer) || (temp >= 60 && temp <= 100 && isSummer);
+    }
+
+    /*
+Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat,
+and a boolean indicating if we are on vacation,
+return a string of the form "7:00" indicating when the alarm clock should ring.
+Weekdays, the alarm should be "7:00" and on the weekend it should be "10:00".
+Unless we are on vacation -- then on weekdays it should be "10:00" and weekends it should be "off".
+
+
+alarmClock(1, false) → "7:00"
+alarmClock(5, false) → "7:00"
+alarmClock(0, false) → "10:00"
+     */
+
+    public String alarmClock(int day, boolean vacation) {
+        if (!vacation) {
+            if (day >= 1 && day <= 5) {
+                return "7:00";
+            } else if (day == 0 || day == 6) {
+                return "10:00";
+            }
+        }
+        if (vacation && day >= 1 && day <= 5) {
+            return "10:00";
+        }
+        return "off";
     }
 }
