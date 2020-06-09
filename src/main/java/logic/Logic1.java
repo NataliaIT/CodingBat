3fc0b2de80b4ff6c1895853e95e8533bb3e640f8
Natalia <natikrak@gmail.com>
2020-06-09 21:09:58 +0200

    less20

--- src/main/java/logic/Logic1.java
+++ src/main/java/logic/Logic1.java
@@ -454,4 +454,19 @@ specialEleven(24) → false
     public boolean specialEleven(int n) {
         return n%11==0 || n%11==1;
     }
+
+    /*
+
+Return true if the given non-negative number is 1 or 2 less than a multiple of 20.
+So for example 38 and 39 return true, but 40 returns false. See also: Introduction to Mod
+
+
+less20(18) → true
+less20(19) → true
+less20(20) → false
+     */
+
+    public boolean less20(int n) {
+        return n % 20 == 19 || n % 20 == 18;
+    }
 }
