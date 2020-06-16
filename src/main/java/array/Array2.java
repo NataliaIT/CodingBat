426a567d95158de2370f151baea2fe81f6f99f56
Natalia <natikrak@gmail.com>
2020-06-16 21:11:01 +0200

    lucky13

--- src/main/java/array/Array2.java
+++ src/main/java/array/Array2.java
@@ -46,4 +46,23 @@ sum13([1, 2, 2, 1, 13]) → 6
         }
         return sum;
     }
+
+    /*
+
+Given an array of ints, return true if the array contains no 1's and no 3's.
+
+
+lucky13([0, 2, 4]) → true
+lucky13([1, 2, 3]) → false
+lucky13([1, 2, 4]) → false
+     */
+
+    public boolean lucky13(int[] nums) {
+        for(int n:nums){
+            if(n==1 || n==3)
+                return false;
+        }
+        return true;
+
+    }
 }
