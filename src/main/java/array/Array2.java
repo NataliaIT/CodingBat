b44595c06997b5e63273d4a3a7ba60e17e742d75
Natalia <natikrak@gmail.com>
2020-06-16 21:47:53 +0200

    matchUp

--- src/main/java/array/Array2.java
+++ src/main/java/array/Array2.java
@@ -104,11 +104,34 @@ no14([2, 3, 4]) → true
 
     public boolean no14(int[] nums) {
         boolean containsNo1=true, containsNo4=true;
-        for (int i : nums) {
-            if(i==1) containsNo1 = false;
-            if(i==4) containsNo4 = false;
+        for (int n : nums) {
+            if(n==1) containsNo1 = false;
+            if(n==4) containsNo4 = false;
 
         }
         return containsNo1 || containsNo4;
+    }
+
+
+    /*
+    Given arrays nums1 and nums2 of the same length, for every element in nums1,
+    consider the corresponding element in nums2 (at the same index).
+    Return the count of the number of times that the two elements differ by 2 or less, but are not equal.
+
+
+matchUp([1, 2, 3], [2, 3, 10]) → 2
+matchUp([1, 2, 3], [2, 3, 5]) → 3
+matchUp([1, 2, 3], [2, 3, 3]) → 2
+     */
+
+    public int matchUp(int[] nums1, int[] nums2) {
+        int count =0;
+
+        for (int i=0;i<nums1.length;i++){
+            if(nums1[i]!=nums2[i] && Math.abs(nums1[i]-nums2[i])<3){
+                count++;
+            }
+        }
+        return count;
     }
 }
