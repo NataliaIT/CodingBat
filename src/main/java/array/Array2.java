1a3a8d79f2ff7dccf355732bb4eb2c9644abc9f8
Natalia <natikrak@gmail.com>
2020-06-22 20:57:48 +0200

    post4

--- src/main/java/array/Array2.java
+++ src/main/java/array/Array2.java
@@ -189,7 +189,7 @@ shiftLeft([1]) → [1
      */
 
     public int[] shiftLeft(int[] nums) {
-        if (nums.length == 0 || nums.length == 1) {
+        if (nums.length < 2) {
             return nums;
         }
         int temp = nums[0];
@@ -199,4 +199,35 @@ shiftLeft([1]) → [1
         nums[nums.length - 1] = temp;
         return nums;
     }
+
+    /*
+
+Given a non-empty array of ints, return a new array containing the elements
+from the original array that come after the last 4 in the original array.
+The original array will contain at least one 4. Note that it is valid in java to create an array of length 0.
+
+
+post4([2, 4, 1, 2]) → [1, 2]
+post4([4, 1, 4, 2]) → [2]
+post4([4, 4, 1, 2, 3]) → [1, 2, 3]
+     */
+    public int[] post4(int[] nums) {
+        int last4 = 0;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] == 4) {
+                last4 = i;
+                break;
+            }
+        }
+
+        int[] resultArray = new int[nums.length - 1 - last4];
+
+        int count = 0;
+        for (int i = last4 + 1; i < nums.length; i++) {
+            resultArray[count] = nums[i];
+            count++;
+        }
+        return resultArray;
+    }
+
 }
