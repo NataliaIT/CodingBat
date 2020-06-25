dea46665611e01be760b3b55e8efe8ae850d13e3
Natalia <natikrak@gmail.com>
2020-06-25 07:08:24 +0200

    sum67

--- src/main/java/array/Array2.java
+++ src/main/java/array/Array2.java
@@ -320,4 +320,33 @@ bigDiff([2, 10, 7, 2]) → 8
     }
 
 
+    /*
+    Return the sum of the numbers in the array, except ignore sections
+    of numbers starting with a 6 and extending to the next 7
+    (every 6 will be followed by at least one 7). Return 0 for no numbers.
+
+
+sum67([1, 2, 2]) → 5
+sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+sum67([1, 1, 6, 7, 2]) → 4
+     */
+    public int sum67(int[] nums) {
+        int sum = 0;
+        boolean section = false;
+
+        for (int i=0; i<nums.length; i++){
+            if(nums[i] == 6){
+                section = true;
+            }
+            if(!section){
+                sum+=nums[i];
+            }
+            else{
+                if(nums[i]==7){
+                    section=false;
+                }
+            }
+        }return sum;
+    }
+
 }
